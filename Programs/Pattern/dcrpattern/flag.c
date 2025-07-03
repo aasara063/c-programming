4864c46c96850aa23066ccdd022b5d6df9474388
aasara063 <aasarabhattarai85@gmail.com>
2025-07-03 23:22:49 +0545

    Flagnp

---
+++ Programs/Pattern/dcrpattern/flag.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    int n, i, j; 
+     for(i = 0; i !=3; i++) {
+        for(j = 1; j <= i+1; j++) {
+            printf("1");
+        }
+            printf("\n");
+    } for(i = 0; i !=6; i++) {
+        for(j = 1; j <= i+1; j++) {
+            printf( "1");
+        }
+            printf("\n");
+    }
+    return 0;
+}
