e156cc508497204a90a6464fa6110d88e0a9b209
aasara063 <aasarabhattarai85@gmail.com>
2025-07-04 23:53:10 +0545

    flag

--- Programs/Pattern/dcrpattern/flag.c
+++ Programs/Pattern/dcrpattern/flag.c
@@ -2,12 +2,12 @@
 int main(){
     printf("pattern of 1's Nepal flag\n");
     int n, i, j; 
-     for(i = 0; i !=3; i++) {
+     for(i = 0; i !=7; i++) {
         for(j = 1; j <= i+1; j++) {
             printf("1");
         }
             printf("\n");
-    } for(i = 0; i !=6; i++) {
+    } for(i = 0; i !=10; i++) {
         for(j = 1; j <= i+1; j++) {
             printf( "1");
         }
